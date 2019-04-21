0d5565f3448fa1b67d1cbff76b1479445409c1b3
Alan Sainz <alanmsainz@gmail.com>
2019-04-21 18:00:23 -0300

    cleans testing code

--- flow-weather-front/src/containers/ForecastContainer/forecastcontainer.test.js
+++ flow-weather-front/src/containers/ForecastContainer/forecastcontainer.test.js
@@ -6,14 +6,17 @@ import { Provider } from "react-redux";
 
 describe("Dropdown Component", () => {
   let wrapper;
-  // our mock login function to replace the one provided by mapDispatchToProps
   const getTodayForecastRequest = jest.fn();
   const getFiveDaysForecastRequest = jest.fn();
 
-   beforeEach(() => {
-     // pass the mock function as the login prop 
-     wrapper = shallow(<ForecastContainer getTodayForecastRequest={getTodayForecastRequest} getFiveDaysForecastRequest={getFiveDaysForecastRequest}/>)
-   })
+  beforeEach(() => {
+   wrapper = shallow(
+     <ForecastContainer 
+       getTodayForecastRequest={getTodayForecastRequest} 
+       getFiveDaysForecastRequest={getFiveDaysForecastRequest}
+     />
+   );
+  });
 
   it('should render with no props', () => {
     expect(wrapper).toMatchSnapshot();
